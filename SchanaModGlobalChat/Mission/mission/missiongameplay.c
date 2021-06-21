7b200a8876559ab8f96e673b75507cac4b9f6b81
DUAL <dual1tyx@hotmail.co.uk>
2021-06-21 22:25:53 +0100

    RPC for role, could probably have been done via Schana chat settings, but wanted to experiment

--- SchanaModGlobalChat/Mission/mission/missiongameplay.c
+++ SchanaModGlobalChat/Mission/mission/missiongameplay.c
@@ -1,15 +1,16 @@
 modded class MissionGameplay extends MissionBase {
     private bool m_SchanaChatChannelIsGlobal = true;
 	
-	
 	void MissionGameplay (){
         GetRPCManager ().AddRPC ("SchanaChat", "SchanaChatSettingsRPC", this, SingleplayerExecutionType.Both);
-		}
+        GetRPCManager ().AddRPC ("SchanaChat", "SchanaRoleColoursRPC", this, SingleplayerExecutionType.Both);
+    }
 	
 	override void OnMissionStart (){
 		super.OnMissionStart ();
 		Print ("[SchanaChat] Requesting settings from server");
 		GetRPCManager ().SendRPC ("SchanaChat", "SchanaChatSettingsRPC", new Param1< SchanaModGlobalChatServerSettings >( NULL ), true, NULL);
+        GetRPCManager ().SendRPC ("SchanaChat", "SchanaRoleColoursRPC", new Param1< array<ref RoleSettingsData> >( NULL ), true, NULL);
 	}
 	
 	void SchanaChatSettingsRPC ( CallType type, ref ParamsReadContext ctx, ref PlayerIdentity sender, ref Object target ) {
@@ -19,6 +20,14 @@ modded class MissionGameplay extends MissionBase {
 		Print ("[SchanaChat] Receiving settings from server");
 		
 		g_SchanaModGlobalChatServerSettings = data.param1;
+	}
+
+    void SchanaRoleColoursRPC (CallType type, ref ParamsReadContext ctx, ref PlayerIdentity sender, ref Object target ) {
+        Param1< array<ref RoleSettingsData> > data  //Player ID, Icon
+		if ( !ctx.Read ( data ) ) return;
+		
+		Print ("[SchanaChat] Receiving roles from server");
+		g_SchanaRoles = data.param1;
 	}
 
 	
